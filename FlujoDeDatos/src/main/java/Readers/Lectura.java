8c022baae0260f3a5c6a103a55f3f8cde44b7b82
JuanMc <mc.loughlin.juan.jose@gmail.com>
2024-03-13 12:42:22 -0300

    llegamnar hasta FIle, setErr

--- FlujoDeDatos/src/main/java/Readers/Lectura.java
+++ FlujoDeDatos/src/main/java/Readers/Lectura.java
@@ -1,29 +1,72 @@
 package Readers;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Lectura {
-	// Stream
+	// Stream  
 	// Stream  IN
 	// Stream  OUT
 	// Stream  ERR
 	
+	PrintStream ps;
+	
 	public Lectura()
 	{
+		//Intermediario entre la escritura y el canal
+		ps = new PrintStream( System.out );
 		
 		System.out.println("Byte:");
 		//Scanner s = new Scanner(System.in);
 		//System.err.println();
 		
 		
-		
+		//byte a byte
 		System.out.write(  13  );  // 
 		System.out.write(  10  );  // enter 
-		System.out.write(  5 );
+		System.out.write(  62 );
 		System.out.write(  27 );
+		
+		
+		//ASCII    UNICODE
+		byte[] array = { 78 , 64 , 119 , 97 , 19 };
+		try {
+			
+			System.out.write( array );
+			
+		} catch (IOException e) {
+			Logger.getLogger( Readers.Lectura.class.getName())
+			.log( Level.SEVERE , null , e );
+			//e.printStackTrace();
+		}
+		
+		System.err.println("ERROR");
+		
 		System.out.flush();
 		//System.out.println();
+		
+		
+		
+		ps.println("holaa");
+		ps.printf("El numero elejido es: %b %d y su nombre: %s en su cuenta tiene: %.2f $ "
+				,true, 8 , "PEPE" ,  655.7);
+		
+		
+		
+		File archivo = new File( "error.log" );
+		
+		PrintStream error = new PrintStream(  );
+		System.setErr( error );
+		
+		System.err.println("esto es un error critico");
 	}
+	
+	
+	
 	
 	
 }
