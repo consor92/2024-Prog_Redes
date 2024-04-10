53e25059c3997c60b0dcced0bd970688eb47cd97
Gonzalo <consor92@gmail.com>
2024-04-10 12:48:27 -0300

    coleccciones y Reader

---
+++ FlujoDeDatos/src/main/java/Readers/Colecciones.java
@@ -0,0 +1,39 @@
+package Readers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Colecciones {
+
+	//vector
+	int[] vec = new int[5];
+	
+	ArrayList<String> frutas = new ArrayList<>();
+	ArrayList verduras = new ArrayList<>();
+	
+	List nombres  = new ArrayList<>();
+	List<String> apellido = new ArrayList<>();
+	
+	LinkedList<String> componentesPc = new LinkedList<>();
+	List precios = new LinkedList<>();
+	
+	LinkedHashMap<String, String> listaEspañolIngles = new LinkedHashMap<>();
+	Map<Integer,Integer> valores = new LinkedHashMap<>();
+	
+	LinkedHashSet<Integer> listaInglesEspañol = new LinkedHashSet<>();
+	Set<Integer> valores2 = new LinkedHashSet<>();	
+	
+	HashMap<String , String> españolRAE = new HashMap<>();
+	Map<String,Integer> españolIngles = new HashMap<>();
+	
+	HashSet<String> claves = new HashSet<>();
+	Set<Integer> DNI = new HashSet<>();
+	
+}
--- FlujoDeDatos/src/main/java/Readers/main.java
+++ FlujoDeDatos/src/main/java/Readers/main.java
@@ -5,49 +5,14 @@ import java.io.PrintStream;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Lectura leer = new Lectura();
+		ClassStrings repaso = new ClassStrings();
+		//repaso.repaso();
 		
-		//String leido = leer.entradaDeDatos();
-		PrintStream ps = new PrintStream( System.out );
-		String texto = "    Este va a ser el, texto <> que yo quiero , manipular.";
-		
-		ps.println("ASCII El char (3):" +  texto.charAt(3) );
-		ps.println("sin espacio:" + texto.trim()  );
-		ps.println("'el' esta en la posicion':" + texto.indexOf("el"));
-		ps.println("el '>' este ne la pocicion:" + texto.indexOf( '>' ) );
-		ps.println("ultima aparicion de la letra E" + texto.lastIndexOf( 'e' ) );
-		ps.println("largo del texto" +  texto.length());
-		
-		ps.println("" + texto.equals( "hola" )   );
-		ps.println("" + texto.compareTo( "hola" )  );
-		ps.println("" + texto.compareToIgnoreCase( "HoLa" ) );
-		
-		ps.println("" + texto.contains( "que" )  );
-		ps.println("" + texto.replace("a", "4") );
-		ps.println("" + texto.replaceAll( " ", "_" ) );
-		
-		ps.println("" + texto.concat(" otro texto" ).concat("otro mas")  );
-		ps.println("" + texto.toLowerCase() );
-		ps.println("" + texto.toUpperCase() );
-		
-		ps.println("" + texto.startsWith("Este") );
-		ps.println("" + texto.endsWith(".") );
-		
-		ps.println("" + texto.substring( 10 ) );
-		ps.println("" + texto.substring( 10 , 20 ) );
-		
-		ps.println("" + texto.substring(  0  ,  texto.indexOf("<>")  ) );
-		ps.println("" + texto );
-		ps.println(" UNICODE (E):" + texto.codePointAt(4) );
-		
-		char[] letraAletra = texto.toCharArray();
-		
-		String separar = "esto, es un texto, que vamos, a separar.";
-		String[] vec = separar.split(",");  //[ "esto" , " es un texto" , " que vamos" , " a separar."]
-		separar.split("\\+");  //   \n   \+ 
-		texto.split(" <> ");
+		ClassReader lectura = new ClassReader();
+		//System.out.println( lectura.leer() );
+			
 	}
 
 }
