01ea27baefbe7f49ec057374b248bb24af1ba96e
Gonzalo Consorti <gonzalo.consorti@bue.edu.ar>
2024-05-22 12:51:04 -0300

    Creacion de archivos

--- Archivos/src/main/java/Archivos/Ficheros.java
+++ Archivos/src/main/java/Archivos/Ficheros.java
@@ -1,41 +1,129 @@
 package Archivos;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Ficheros {
-	
+
 	private File archivo;
 	private PrintStream ps;
-	
-	public Ficheros()
-	{
-		String ruta = "";  //   " c:\\User\\ "
-		String nombre = "mario";
-		String extension = ".txt";
-		
-		archivo =  new File( ruta.concat(nombre.concat(extension))  );
-	}
-	
-	public void createFilePrintStream(File a)
-	{
-		FileOutputStream fos = null;
+
+	public Ficheros(String rut, String name , String ext) {
+		String ruta = rut; //  " C:\\User\\ "
+		String nombre = name;
+		String extension = ext;
 		
 		try {
-			fos = new FileOutputStream(a);
-			ps = new PrintStream(fos , true); //mode append true = no sobreescribe
-			
+			System.setErr( new PrintStream(
+						   new FileOutputStream(
+						   new File("Errores.log")) , true) 
+						 );
+		}catch(FileNotFoundException e) {
+			Logger.getLogger( Ficheros.class.getName() ).log(Level.WARNING,null, e);
+		}
+		archivo = new File(ruta.concat(nombre.concat(extension)));
+	}
+
+	public File getArchivo()
+	{
+		return this.archivo;
+	}
+	
+	public void createFilePrintStream(File a) {
+		FileOutputStream fos = null;
+
+		try {
+			fos = new FileOutputStream(a ,true);
+			ps = new PrintStream(fos); // mode append true = no sobreescribe
+
 			ps.println("holaa mundo");
 			ps.println("Chauu mundo");
-			
+
 			ps.flush();
-			
+
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			Logger.getLogger( Ficheros.class.getName() ).log(Level.WARNING,null, e);
+		} finally {
+			if (fos != null)
+				try {
+					fos.close();
+				} catch (IOException e) {
+					Logger.getLogger( Ficheros.class.getName() ).log(Level.WARNING,null, e);
+				}
+		}
+	}
+
+	public void createFilePrinter(File a)
+	{
+		FileWriter fw = null;
+		PrintWriter pw = null;
+
+		try {
+			if( !a.exists() )
+			{
+				a.createNewFile();
+			}			
+			
+			fw = new FileWriter( a , true);
+			pw = new PrintWriter(fw);
+			
+			pw.println("Otro MUNDO");		
+		} catch (FileNotFoundException e ) {
+			Logger.getLogger( Ficheros.class.getName() ).log(Level.WARNING,null, e);
+		} catch (IOException e ){
+			Logger.getLogger( Ficheros.class.getName() ).log(Level.WARNING,null, e);
+		}
+		finally {
+			try {
+				if( pw != null)
+					pw.close();
+				
+				if( fw != null)
+					fw.close();
+			} catch (IOException e) {
+				Logger.getLogger( Ficheros.class.getName() ).log(Level.WARNING, null, e);
+			}
+		}
+	}
+	
+	public void createFileBuffered(File a)
+	{
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+		
+		try {
+			fw = new FileWriter(a , true);
+			bw = new BufferedWriter(fw);
+			
+			bw.write("Explotemos al MUNDO");
+			bw.newLine();
+			bw.write("Mejor creemos un nuevo MUNDO");
+			
+			bw.flush();
+		} catch (IOException e) {
+			Logger.getLogger( Ficheros.class.getName() ).log(Level.WARNING,null, e);
+		}finally {
+			try {
+				if( fw != null )
+					fw.close();
+				
+				if(bw != null)
+					bw.close();
+				
+			} catch (IOException e) {
+				Logger.getLogger( Ficheros.class.getName() ).log(Level.WARNING,null, e);	 			
+			}
 		}
 		
 	}
-	
 }
+
+
--- Archivos/src/main/java/Archivos/main.java
+++ Archivos/src/main/java/Archivos/main.java
@@ -4,6 +4,10 @@ public class main {
 
 	public static void main(String[] args) {
 		
+		Ficheros archivo = new Ficheros("","Mario",".txt");
+		
+		//archivo.createFilePrintStream( archivo.getArchivo() );
+		archivo.createFilePrinter( archivo.getArchivo() );
 	}
 
 }
