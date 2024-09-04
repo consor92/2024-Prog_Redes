0d0ebebf1ed7fb24e0376d6f06414ddc9b7e6426
Gonzalo Consorti <gonzalo.consorti@bue.edu.ar>
2024-09-04 14:32:07 -0300

    Armando estructura clase main, transfeencia y banco

--- Archivos/src/main/java/Banco/Colores.java
+++ Archivos/src/main/java/Banco/Colores.java
@@ -1,6 +1,6 @@
-package Utils;
+package Banco;
 
-class utilizades {
+class Colores {
 
 	public static final String ANSI_BLACK = "\u001B[30m";
 	public static final String ANSI_RED = "\u001B[31m";
@@ -17,8 +17,9 @@ class utilizades {
 	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
 	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
 	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
-	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m"
-
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+	
 	public static final String ANSI_RESET = "\u001B[0m";
 
 }
